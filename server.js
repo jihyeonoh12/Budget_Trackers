11893a85c2c356e9fe2d3de585245de61da889fc
jihyeonoh12 <jihyeonoh12@gmail.com>
2020-04-22 21:13:13 -0700

    final

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
 const app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
@@ -15,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb+srv://jane-admin:Test123@budgettrackerdb-voanx.mongodb.net/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
